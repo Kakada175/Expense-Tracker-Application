d769c5644a8d514c4f4f8cf54c49b04a1bb24e87
Sathya <moeunsathya@gmail.com>
2026-01-17 22:21:54 +0700

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,26 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/material.dart';
+import 'screens/account_screen.dart';
+import 'screens/login_screen.dart';
+//import 'screens/account_screen.dart';
 
+
+void main() => runApp(DevicePreview(builder: (context) => MyApp()));
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Auth UI',
+      theme: ThemeData(
+        textTheme: Typography.blackCupertino,
+        primaryColor: Colors.blueAccent,
+      ),
+      home: const LoginScreen(),
+   //   home: const AccountScreen(),
+    );
+  }
+}
